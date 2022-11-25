cd46934cddef9f63b018146ccb12f0c3345e392d
Sachin <Sachin@Syadav>
2022-11-25 20:40:14 +0530

    Decision Making

---
+++ JavaDemo/src/Demo9/Hello9.java
@@ -0,0 +1,17 @@
+package Demo9;
+
+public class Hello9 {
+
+	public static void main(String[] args) {
+		int x = 8, y = 7;
+		
+        if(x >= y)
+        {
+     	   System.out.println("true");
+        }
+        else
+        {
+     	   System.out.println("false");
+        }
+	}
+}
