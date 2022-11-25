ef5005e0ea7e87b8b30aa75452ffd00f0cbf4e1a
Sachin <Sachin@Syadav>
2022-11-25 20:25:24 +0530

    Typecasting

---
+++ JavaDemo/src/Demo6/Hello6.java
@@ -0,0 +1,10 @@
+package Demo6;
+
+public class Hello6 {
+
+	public static void main(String[] args) {
+	       float f = 10.532f;
+	       long l = (long) f;
+	       System.out.println(l);
+		}
+	}
