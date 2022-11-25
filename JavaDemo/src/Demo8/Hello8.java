ded9d45a9fffdbc4b3f72ed815a23130fbcbb21a
Sachin <Sachin@Syadav>
2022-11-25 20:35:03 +0530

    Operators

---
+++ JavaDemo/src/Demo8/Hello8.java
@@ -0,0 +1,17 @@
+package Demo8;
+
+public class Hello8 {
+
+	public static void main(String[] args) {
+		int x = 5;
+        if(x != 5)
+        {
+      	  System.out.println("Value of x is not 5");
+      	  
+        }
+        else
+        {
+        System.out.println("Value of x is 5");
+        }
+	}
+}
