19fe6555e70d91d957791611f9f2c73128548198
Sachin <Sachin@Syadav>
2022-11-26 10:31:26 +0530

    Assignment

---
+++ JavaDemo/src/Demo12/Hello12.java
@@ -0,0 +1,14 @@
+package Demo12;
+
+public class Hello12 {
+
+	public static void main(String[] args) {
+		int x = 4;
+		//x += 5; // x = x + 5
+		//x -= 5; // x = x - 5
+		//x *= 5; // x = x * 5
+		//x /= 4; // x = x / 4
+		x %= 5;   // x = x % 5
+		System.out.println(x);
+	}
+}
