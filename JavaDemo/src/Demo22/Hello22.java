5106f53653216d074b3ec62264466646bd7d615d
Sachin <Sachin@Syadav>
2022-11-26 10:57:38 +0530

    Nested Loop

---
+++ JavaDemo/src/Demo22/Hello22.java
@@ -0,0 +1,15 @@
+package Demo22;
+
+public class Hello22 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++)
+		{
+			for(int j=1; j<=10; j++)
+			{
+	System.out.println("Value of i:"+i+" and value of j:"+j);
+			}
+			System.out.println("************************");
+		}
+	}
+}
